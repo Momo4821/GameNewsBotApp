4bccb2d358e6fd0581503dc6129d1e2d6d710b0d
Malania <wilsonmcw4@gmail.com>
2025-06-29 19:45:37 -0400

    Working on String Formatter

--- Commands/Basic_Commands.cs
+++ Commands/Basic_Commands.cs
@@ -64,5 +64,40 @@ namespace GameNewsBotApp.Commands
 
         }
 
+
+     /*   public class Reminder_Command : BaseCommandModule
+        {
+            [Command("Reminder")]
+            [Description("Set a reminder for yourself")]
+            public Task _Reminder_Command(CommandContext _Command_Reminder)
+            {
+
+                //timer for reminder
+                var reminderTime = TimeSpan.FromMinutes(15) // Set reminder for 15 minute later//remindfer for 4 days
+                    .Add(TimeSpan.FromHours(6)) //add 6 hours
+                    .Add(TimeSpan.FromMinutes(30)) //add 30 minutes
+                    .Add(TimeSpan.FromDays(7)); //days
+                                                //user sets reminder for 7 days later
+                                                var reminderMessage = $"Reminder set for {_Command_Reminder.User.Username} in {reminderTime.TotalMinutes} minutes!";
+                                                _Command_Reminder.RespondAsync(reminderMessage);
+                                                //remove remin
+
+
+                return Task.CompletedTask;
+            }*/
+
+      /*      public class othercommands : BaseCommandModule
+
+            {
+        [Command("Say")]
+            public async Task Say(string choice)
+            {
+               
+            }
+
+
+            }*/
+            
+        }
     }
 }
--- Commands/News_Command.cs
+++ Commands/News_Command.cs
@@ -14,12 +14,28 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using static GameNewsBotApp.Logging.Logging;
 
 namespace GameNewsBotApp.Commands
 {
+
+
+
+    //string formate
+  
+
+
+
     internal class News_Command
     {
+
+
+    
+
+
+
+     
 
 
 
@@ -42,12 +58,13 @@ namespace GameNewsBotApp.Commands
 
 
             private static readonly HttpClient _httpClient = new HttpClient();// HttpClient instance to make requests to the Steam API. URL will be the same for this call
-            private const string ChannelId = "1384707760716779651"; // Replace with your actual channel ID
+            private const ulong ChannelId_News = 1384707760716779651; 
+        
 
             private const string NewsApiUrl =
                 "https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=3&maxlength=300&format=json"; // URL for the Steam News API for Team Fortress 2 (appid=440) with parameters for count and maxlength
 
-            [Command("NewsTest")] 
+            [Command("TF2")] 
             [Description("Ping Command that pings user and dispalys latency")]
             public async Task News_Command(CommandContext _command_News)
 
@@ -57,47 +74,73 @@ namespace GameNewsBotApp.Commands
 
 
 
-                if (_command_News.Channel.Id != 1384707760716779651) //only send message to a specifc channel using an if statement
+                if (_command_News.Channel.Id != ChannelId_News) //ONLY WANT TO SEND TO NEWS CHANNEL BECUASE DON'T SPAM OTHER GENERAL CHANNELS 
                 {
-                    _command_News.RespondAsync("Inccorect Channel")
+                     await _command_News.RespondAsync("Inccorect Channel");
                 }
-
-
-                try
+                else
                 {
-                    var response = await _httpClient.GetStringAsync(NewsApiUrl);  // Make a GET request to the Steam News API
-                    var json = JObject.Parse(response); // Parse the JSON response into a JObject
-                    var appnews = json["appnews"]["newsitems"].ToObject<List<Newsitem>>(); // Deserialize the news items into a list of Newsitem objects
-                    //use string.join to format the news items
-                    var newsContent =  // Format the news items into a string for display
-                        string.Join("\n\n", appnews.Select(item =>
-                        $"**Title:** {item.title}\n**Author:** {item.author}\n**Contents:** {item.contents}\n**URL:** {item.url}"));
-
-                    
 
 
+                    try
+                    {
+                        var response =
+                            await _httpClient.GetStringAsync(NewsApiUrl); 
+                        var json = JObject.Parse(response); 
+                        var appnews = json["appnews"]["newsitems"].ToObject<List<Newsitem>>(); // Deserialize the news items into a list of Newsitem objects
+                        //use string.join to format the news items
+                        var newsContent = // Format the news items into a string for display
+                            string.Join("\n\n", appnews.Select(item =>
+                                $"**Title:** {item.title}\n**Author:** {item.author}\n**Contents:** {item.contents}\n**URL:** {item.url}"));
 
-                    _command_News.RespondAsync(
-                        $"Here are the latest news items:\n\n{newsContent}"); // Respond to the command with the formatted news items
 
+
+
+
+                        await _command_News.RespondAsync(
+                            $"Here are the latest news items:\n\n{newsContent}"); // Respond to the command with the formatted news items
+
+
+                    }
+                    catch (Exception)
+                    {
+                        var time_stamp = new TimestampAttribute();
+                        /*Discord_logger_service.Log_Critical($"Request Failed{stack_trace}");*/ // stacktrace not neded here but can be used for debugging
+                        /*   var log_file = new StreamWriter(File.Create($"C:/Log{time_stamp}")*/
+                        ;
+                        throw new ArgumentException("Access Denied");
+                    }
 
                 }
-                catch (Exception d)
-                {
-                    var time_stamp = new TimestampAttribute();
-                    Discord_logger_service.Log_Critical($"Request Failed{stack_trace}"); // Log the error with a timestamp
-                    var lo_file = new StreamWriter(File.Create($"C:/Log{time_stamp}"))
-                    ;
-                    throw new ArgumentException("Access Denied");
-                }
-
-
 
             }
 
 
 
         }
+
+
+
+        public class Marvel_rivals : BaseCommandModule
+        {
+            public Task Marvel_Comamnd (CommandContext _command_Marvel)
+            {
+               
+
+
+
+
+                return Task.CompletedTask;
+            }
+            {
+
+                // This is a placeholder for the Marvel rivals command
+
+            }
+
+
+        }
+
     }
 
 
